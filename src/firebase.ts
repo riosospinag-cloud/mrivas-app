91ce558873145bc7d0416f7d904b1646535f961b
Geraldine <tu-correo-de-github@gmail.com>
2026-04-30 14:01:11 -0500

    fix firebase api key

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPQLOPoFTJSqSt7I5xSaq9GAaq9RqJLuI",
+  apiKey: "AIzaSyBPQLOPofTJSqSt7I5xSaq9GAaq9RqJLuI",
   authDomain: "mrivas-app.firebaseapp.com",
   projectId: "mrivas-app",
   storageBucket: "mrivas-app.firebasestorage.app",
