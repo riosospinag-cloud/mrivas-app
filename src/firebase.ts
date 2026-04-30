907a911e1432ed042083aa0e8c35ed23423be321
Geraldine <tu-correo-de-github@gmail.com>
2026-04-30 13:23:36 -0500

    Corrige api key Firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,9 @@
 import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
+import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
+// 🔥 CONFIGURACIÓN REAL DE TU PROYECTO
 const firebaseConfig = {
-  apiKey: "AIzaSyBPQLOPoFTJSgSt7I5xSaq9GAaq9RqJLuI",
+  apiKey: "AIzaSyBPQLOPoFTJSqSt7I5xSaq9GAAq9RqJLuI",
   authDomain: "mrivas-app.firebaseapp.com",
   projectId: "mrivas-app",
   storageBucket: "mrivas-app.firebasestorage.app",
@@ -10,6 +11,11 @@ const firebaseConfig = {
   appId: "1:498206141256:web:deca23ace7389ffc4ca17d"
 }
 
+// 🔥 INICIALIZAR FIREBASE
 const app = initializeApp(firebaseConfig)
 
+// 🔥 AUTH (LOGIN)
 export const auth = getAuth(app)
+
+// 🔥 PROVEEDOR GOOGLE
+export const provider = new GoogleAuthProvider()
