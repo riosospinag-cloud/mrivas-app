ee6a27873368f91344cb5e041ee7e188fbd1542c
Geraldine <tu-correo-de-github@gmail.com>
2026-05-04 00:29:42 -0500

    Fix firebase definitivo

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
+// 🔥 Configuración correcta de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBPQLOPoFTJSqSt7I5xSaq9GAaq9RqJLuI",
   authDomain: "mrivas-app.firebaseapp.com",
@@ -10,7 +11,9 @@ const firebaseConfig = {
   appId: "1:498206141256:web:deca23ace7389ffc4ca17d"
 }
 
+// 🔥 Inicializar Firebase
 const app = initializeApp(firebaseConfig)
 
+// 🔥 Exportar autenticación
 export const auth = getAuth(app)
 export const provider = new GoogleAuthProvider()
